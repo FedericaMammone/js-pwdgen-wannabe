1fb40c75154e4e0e11c20a1a158acf8bf9b85508
Fef97 <federicakr97@gmail.com>
2023-04-26 15:04:33 +0200

    Inseriti commenti

---
+++ JS/main.js
@@ -0,0 +1,16 @@
+// le mie variabili
+
+
+// chiediamo all'utente il suo nome
+
+
+// chiediamo all'utente il suo cognome
+
+
+// chiediamo all'utente il suo colore preferito
+
+
+// creiamo messaggio per l'utente
+
+
+// e gli diciamo quale sarà la sua password
